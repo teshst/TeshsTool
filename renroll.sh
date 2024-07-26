673f86129b90d4aeb3363a00fd9eecffd5e7cd0e
TeshST <14990628+teshst@users.noreply.github.com>
2024-07-26 14:11:54 -0600

    Update renroll.sh

--- renroll.sh
+++ renroll.sh
@@ -12,8 +12,8 @@ fi
 # Ensure the target directory exists
 TARGET_DIR="$VOLUME/var/db/ConfigurationProfiles"
 if [ ! -d "$TARGET_DIR" ]; then
-  echo "Target directory '$TARGET_DIR' does not exist. Creating directory."
-  mkdir -p "$TARGET_DIR"
+  echo "Error: Target directory '$TARGET_DIR' does not exist."
+  exit 1
 fi
 
 # Remove existing files in the specified directory
