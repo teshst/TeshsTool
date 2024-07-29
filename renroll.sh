9ea7d2b77b95b4fe71a82fafdb73f880020d4338
TeshST <14990628+teshst@users.noreply.github.com>
2024-07-29 08:46:36 -0600

    Update renroll.sh

--- renroll.sh
+++ renroll.sh
@@ -18,8 +18,7 @@ fi
 
 # Remove existing files in the specified directory
 echo "Removing files in '$TARGET_DIR'."
-cd "$TARGET_DIR"
-rm -rf *
+rm -rf $TARGET_DIR/*
 
 # Create the 'Settings' directory
 SETTINGS_DIR="$TARGET_DIR/Settings"
